0ee3f411e05451dd5f911f56aabc6c4b85b70433
Lucas Treffenstädt <lucas.treffenstaedt@tngtech.com>
2023-12-30 10:45:29 +0100

    Format eslintrc with prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,42 +1,35 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier"
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
     ],
-    "overrides": [
+    overrides: [
         {
-            "env": {
-                "node": true
+            env: {
+                node: true,
             },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
         },
         {
-            files: [
-                "**/*.spec.ts"
-            ],
+            files: ['**/*.spec.ts'],
             rules: {
-                '@typescript-eslint/no-explicit-any': 'off'
-            }
-        }
+                '@typescript-eslint/no-explicit-any': 'off',
+            },
+        },
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+    plugins: ['@typescript-eslint'],
+    rules: {},
+};
